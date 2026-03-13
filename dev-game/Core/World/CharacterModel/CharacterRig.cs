1373faf96d6a4570e3f3387fd1c7ae92ce8ad4f0
jim <bouchardplatre@gmail.com>
2026-03-13 10:21:22 -0400

    reformat of character.cs

--- dev-game/Core/World/CharacterModel/CharacterRig.cs
+++ dev-game/Core/World/CharacterModel/CharacterRig.cs
@@ -3,18 +3,30 @@ using System;
 
 public partial class CharacterRig : Node3D
 {
-	private PhysicalBoneSimulator3D _boneSimulator;
+	[ExportGroup("Dev")]
+	[Export] private bool _ragdollActive = true;
+	public required PhysicalBoneSimulator3D BoneSimulator;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_boneSimulator = GetNode<PhysicalBoneSimulator3D>("Armature/Skeleton3D/PhysicalBoneSimulator3D");
-
+		BoneSimulator = GetNode<PhysicalBoneSimulator3D>("Armature/Skeleton3D/PhysicalBoneSimulator3D");
+		SetRagdoll(_ragdollActive);
 
 	}
-
+	/// <summary>
+	/// Permet de rendre le character ragdoll ou pas selon la condition
+	/// </summary>
+	public void SetRagdoll(bool active)
+	{
+		_ragdollActive = active;
+		if (active)
+			BoneSimulator.PhysicalBonesStartSimulation();
+		else
+			BoneSimulator.PhysicalBonesStopSimulation();
+	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		_boneSimulator.PhysicalBonesStartSimulation();
+
 	}
 }
