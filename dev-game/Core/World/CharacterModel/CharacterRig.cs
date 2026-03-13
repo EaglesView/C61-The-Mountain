4dd999c000ca08a0ffe48d8ecafe8c267937d735
jim <bouchardplatre@gmail.com>
2026-03-13 08:37:42 -0400

    ajout du characterRig pour activeRagdoll

---
+++ dev-game/Core/World/CharacterModel/CharacterRig.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class CharacterRig : Node3D
+{
+    private PhysicalBoneSimulator3D _boneSimulator;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _boneSimulator = GetNode<PhysicalBoneSimulator3D>("Armature/Skeleton3D/PhysicalBoneSimulator3D");
+
+
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        _boneSimulator.PhysicalBonesStartSimulation();
+    }
+}
