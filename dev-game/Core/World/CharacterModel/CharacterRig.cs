09cfd058fa92a5329b042050cff051f7cc544598
jim <bouchardplatre@gmail.com>
2026-03-13 08:43:13 -0400

    Ajout des scènes Gym de développement

--- dev-game/Core/World/CharacterModel/CharacterRig.cs
+++ dev-game/Core/World/CharacterModel/CharacterRig.cs
@@ -3,18 +3,18 @@ using System;
 
 public partial class CharacterRig : Node3D
 {
-    private PhysicalBoneSimulator3D _boneSimulator;
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        _boneSimulator = GetNode<PhysicalBoneSimulator3D>("Armature/Skeleton3D/PhysicalBoneSimulator3D");
+	private PhysicalBoneSimulator3D _boneSimulator;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_boneSimulator = GetNode<PhysicalBoneSimulator3D>("Armature/Skeleton3D/PhysicalBoneSimulator3D");
 
 
-    }
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        _boneSimulator.PhysicalBonesStartSimulation();
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		_boneSimulator.PhysicalBonesStartSimulation();
+	}
 }
