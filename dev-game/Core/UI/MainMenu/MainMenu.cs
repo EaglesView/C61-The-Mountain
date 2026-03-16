5645ea436ca5b5d81b6f2ad6e73becadd6096880
s.bergeron <hotrixtak4532@gmail.com>
2026-03-16 13:51:31 -0400

    Login

---
+++ dev-game/Core/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,58 @@
+using Godot;
+
+public partial class MainMenu : Control
+{
+    private Button _play = null!;
+    private Button _create = null!;
+    private Button _join = null!;
+    private Button _profile = null!;
+    private Button _credits = null!;
+    private Button _exit = null!;
+
+    public override void _Ready()
+    {
+        _play = GetNode<Button>("VBoxContainer/PlayButton");
+        _create = GetNode<Button>("VBoxContainer/CreateGameButton");
+        _join = GetNode<Button>("VBoxContainer/JoinGameButton");
+        _profile = GetNode<Button>("VBoxContainer/ProfileButton");
+        _credits = GetNode<Button>("VBoxContainer/CreditsButton");
+        _exit = GetNode<Button>("VBoxContainer/ExitButton");
+
+        _play.Pressed += OnPlayPressed;
+        _create.Pressed += OnCreatePressed;
+        _join.Pressed += OnJoinPressed;
+        _profile.Pressed += OnProfilePressed;
+        _credits.Pressed += OnCreditsPressed;
+        _exit.Pressed += OnExitPressed;
+    }
+
+    private void OnPlayPressed()
+    {
+        GetTree().ChangeSceneToFile("res://Core/Dev/world_jim.tscn");
+    }
+
+    private void OnCreatePressed()
+    {
+
+    }
+
+    private void OnJoinPressed()
+    {
+
+    }
+
+    private void OnProfilePressed()
+    {
+
+    }
+
+    private void OnCreditsPressed()
+    {
+
+    }
+
+    private void OnExitPressed()
+    {
+        GetTree().Quit();
+    }
+}
