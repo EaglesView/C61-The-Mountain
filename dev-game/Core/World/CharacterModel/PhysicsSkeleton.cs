e6d36c1c29c11d8e2de84ad5710234c77f0cd4ba
jim <bouchardplatre@gmail.com>
2026-03-19 12:22:25 -0400

    bugfixes abt comment of pull request #14

--- dev-game/Core/World/CharacterModel/PhysicsSkeleton.cs
+++ dev-game/Core/World/CharacterModel/PhysicsSkeleton.cs
@@ -40,6 +40,7 @@ public partial class PhysicsSkeleton : Skeleton3D
     [Export(PropertyHint.Range, "0.0f,200.0f,1,0f")] public float LinearSpringDamping = 40.0f;
     [Export(PropertyHint.Range, "0.0f,10000.0f,1,0f")] public float AngularSpringStiffness = 4000.0f;
     [Export(PropertyHint.Range, "0.0f,200.0f,1,0f")] public float AngularSpringDamping = 80.0f;
+    [Export] public bool IsRagdoll = false;
     private PhysicalBoneSimulator3D? _boneSim;
     private List<PhysicalBone3D>? _physicsBones;
     private int _headBoneIdx;
@@ -51,97 +52,108 @@ public partial class PhysicsSkeleton : Skeleton3D
     [Export] public required AnimationPlayer AnimPlayer;
 
     /// <summary>
-    /// Oriente la tête du personnage selon l'angle vertical de la caméra.
-    /// xAngle en radians.
-    /// </summary>
-    public void SetHeadPose(float InHeadXAngle)
-    {
+	/// Oriente la tête du personnage selon l'angle vertical de la caméra.
+	/// xAngle en radians.
+	/// </summary>
+	public void SetHeadPose(float InHeadXAngle)
+	{
+		var headRot = new Quaternion(Vector3.Right, -InHeadXAngle);
+		var mouthRot = new Quaternion(Vector3.Right, -InHeadXAngle + (MathF.PI / 2.0f));
+		SetBonePoseRotation(_headBoneIdx, headRot);
+		SetBonePoseRotation(_mouthBoneIdx, mouthRot);
+		SetSpinePoseFromHead();
+	}
+	public void SetSpinePoseFromHead(){
+	TargetSkeleton.SetBonePoseRotation(_spine3BoneIdx, new Quaternion(Vector3.Right, -HeadAngle * 1.0f));
+	TargetSkeleton.SetBonePoseRotation(_spine4BoneIdx, new Quaternion(Vector3.Right, -HeadAngle * 0.4f));
+	}
+	/// ···········································
+	/// : _    ___ ___ ___ _____   _____ _    ___ :
+	/// :| |  |_ _| __| __/ __\ \ / / __| |  | __|:
+	/// :| |__ | || _|| _| (__ \ V | (__| |__| _| :
+	/// :|____|___|_| |___\___| |_| \___|____|___|:
+	/// ···········································
+	public override void _Ready()
+	{
+		ProcessPriority = 1;
 
-        var headRot = new Quaternion(Vector3.Right, -InHeadXAngle);
-        var mouthRot = new Quaternion(Vector3.Right, -InHeadXAngle + (MathF.PI / 2.0f));
-        SetBonePoseRotation(_headBoneIdx, headRot);
-        SetBonePoseRotation(_mouthBoneIdx, mouthRot);
-        //SetBonePoseRotation(FindBone("Spine.004"), headRot / 2);
+		_boneSim = GetNode<PhysicalBoneSimulator3D>("PhysicalBoneSimulator3D");
+		//TARGET BONES FOR SPECIFIC CONTROLS
+		// SPINE ET HEAD POUR LA ROTATION DE LA TETE
+		// ARMS pour POINTER
+		_headBoneIdx = TargetSkeleton.FindBone("Head.001");
+		_spine3BoneIdx = TargetSkeleton.FindBone("Spine.003");
+		_spine4BoneIdx = TargetSkeleton.FindBone("Spine.004");
+		_mouthBoneIdx = TargetSkeleton.FindBone("Mouth.001");
+		_physicsBones = _boneSim.GetChildren()
+		.OfType<PhysicalBone3D>()
+		.ToList();
+		var bonesToSimulate = new Godot.Collections.Array<StringName>();
+		foreach (PhysicalBone3D bone in _physicsBones)
+		{
+			
+		bonesToSimulate.Add(new StringName(GetBoneName(bone.GetBoneId())));
 
+		}
+		_boneSim.PhysicalBonesStartSimulation(bonesToSimulate);
+	}
 
-    }
-    /// ···········································
-    /// : _    ___ ___ ___ _____   _____ _    ___ :
-    /// :| |  |_ _| __| __/ __\ \ / / __| |  | __|:
-    /// :| |__ | || _|| _| (__ \ V | (__| |__| _| :
-    /// :|____|___|_| |___\___| |_| \___|____|___|:
-    /// ···········································
-    public override void _Ready()
-    {
-        ProcessPriority = 1;
+	public override void _PhysicsProcess(double delta)
+	{
+	if(IsRagdoll) return;
+		if (_physicsBones == null) return;
+		foreach (PhysicalBone3D bone in _physicsBones)
+		{
+			float LinStiff = bone.HasMeta("LinearStiffness")  ? (float)bone.GetMeta("LinearStiffness")  : LinearSpringStiffness;
+			float LinDamp = bone.HasMeta("LinearDamping")    ? (float)bone.GetMeta("LinearDamping")    : LinearSpringDamping;
+			float RotStiff =  bone.HasMeta("AngularStiffness") ? (float)bone.GetMeta("AngularStiffness") : AngularSpringStiffness;
+			float RotDamp = bone.HasMeta("AngularDamping")   ? (float)bone.GetMeta("AngularDamping")   : AngularSpringDamping;
+			//LINEAR START
+			//On ramasse les transformations du rig animé et ceux du rig physique
+			Transform3D TransformTarget = TargetSkeleton.GlobalTransform * TargetSkeleton.GetBoneGlobalPose(bone.GetBoneId());
+			Transform3D TransformCurrent = bone.GlobalTransform * bone.BodyOffset.AffineInverse();//* GetBoneGlobalPose(bone.GetBoneId());
 
-        _boneSim = GetNode<PhysicalBoneSimulator3D>("PhysicalBoneSimulator3D");
-        _headBoneIdx = TargetSkeleton.FindBone("Head.001");
-        _spine3BoneIdx = TargetSkeleton.FindBone("Spine.003");
-        _spine4BoneIdx = TargetSkeleton.FindBone("Spine.004");
-        _mouthBoneIdx = TargetSkeleton.FindBone("Mouth.001");
-        _physicsBones = _boneSim.GetChildren()
-        .OfType<PhysicalBone3D>()
-        .ToList();
-        var bonesToSimulate = new Godot.Collections.Array<StringName>();
-        foreach (PhysicalBone3D bone in _physicsBones)
-        {
-            {
-                GD.Print(bone.Name);
-                bonesToSimulate.Add(new StringName(GetBoneName(bone.GetBoneId())));
-            }
+			//On ramasse la différence, et on applique une force selon sa distance et vélocité actuelle
+			Vector3 PositionDifference = TransformTarget.Origin - TransformCurrent.Origin;
+			Vector3 Force = HookesLaw(PositionDifference, bone.LinearVelocity, LinStiff, LinDamp);
+			// LINEAR APPLY
+			bone.LinearVelocity += Force * (float)delta; //linear
+			//ANGULAR START
+			Basis skeletonInv = GlobalTransform.Basis.Inverse();
+			Quaternion targetRot = (skeletonInv * TransformTarget.Basis).GetRotationQuaternion().Normalized();
+			Quaternion currentRot = (skeletonInv * TransformCurrent.Basis).GetRotationQuaternion().Normalized();
+			//Correction Bug Shortest Path, évite les rotations douteuses
+			if (targetRot.Dot(currentRot) < 0f)
+				currentRot = -currentRot;
 
-        }
-        _boneSim.PhysicalBonesStartSimulation(bonesToSimulate);
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-        if (_physicsBones == null) return;
-        foreach (PhysicalBone3D bone in _physicsBones)
-        {
-            //LINEAR START
-            //On ramasse les transformations du rig animé et ceux du rig physique
-            Transform3D TransformTarget = TargetSkeleton.GlobalTransform * TargetSkeleton.GetBoneGlobalPose(bone.GetBoneId());
-            Transform3D TransformCurrent = bone.GlobalTransform * bone.BodyOffset.AffineInverse();//* GetBoneGlobalPose(bone.GetBoneId());
-
-            //On ramasse la différence, et on applique une force selon sa distance et vélocité actuelle
-            Vector3 PositionDifference = TransformTarget.Origin - TransformCurrent.Origin;
-            Vector3 Force = HookesLaw(PositionDifference, bone.LinearVelocity, LinearSpringStiffness, LinearSpringDamping);
-            // LINEAR APPLY
-            bone.LinearVelocity += Force * (float)delta; //linear
-            //ANGULAR START
-            Basis skeletonInv = GlobalTransform.Basis.Inverse();
-            Quaternion targetRot = (skeletonInv * TransformTarget.Basis).GetRotationQuaternion().Normalized();
-            Quaternion currentRot = (skeletonInv * TransformCurrent.Basis).GetRotationQuaternion().Normalized();
-
-            Quaternion rotDiff = (targetRot * currentRot.Inverse()).Normalized();
-            //Le displacement angulaire est juste une fancy différence de rotation précise
-            Vector3 angularDisplacement = rotDiff.GetAxis() * rotDiff.GetAngle();
-
-            // Utiliser le localspace pour l'AngularVelocity et le Torque
+			Quaternion rotDiff = (targetRot * currentRot.Inverse()).Normalized();
+			//Le displacement angulaire est juste une fancy différence de rotation précise
+			//Correction pour les erreurs de floating point
+			float angle = rotDiff.GetAngle();
+			Vector3 angularDisplacement = (angle > 1e-4f) ? rotDiff.GetAxis() * angle : Vector3.Zero;
+			// Utiliser le localspace pour l'AngularVelocity et le Torque
             Vector3 localAngVel = skeletonInv * bone.AngularVelocity;
-            Vector3 localTorque = HookesLaw(angularDisplacement, localAngVel, AngularSpringStiffness, AngularSpringDamping);
+            Vector3 localTorque = HookesLaw(angularDisplacement, localAngVel, RotStiff, RotDamp);
 
-            // Reconvertir en worldspace pour l'appliquer
-            // ANGULAR APPLY
-            bone.AngularVelocity += GlobalTransform.Basis * (localTorque * (float)delta);
-        }
+			// Reconvertir en worldspace pour l'appliquer
+			// ANGULAR APPLY
+			bone.AngularVelocity += GlobalTransform.Basis * (localTorque * (float)delta);
+		}
 
-    }
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        //Process roule apres physicsprocess(), et a cause de
-        //         ProcessPriority = 1;
-        // je peux set les angles et les forces que le player envoie avant le reste
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		//Process roule apres physicsprocess(), et a cause de
+		//         ProcessPriority = 1;
+		// je peux set les angles et les forces que le player envoie avant le reste
 
-        //Ajuster les bones en dessous de la tete selon la rotation du joueur
-        TargetSkeleton.SetBonePoseRotation(_spine3BoneIdx, new Quaternion(Vector3.Right, -HeadAngle * 0.8f));
-        TargetSkeleton.SetBonePoseRotation(_spine4BoneIdx, new Quaternion(Vector3.Right, -HeadAngle * 0.4f));
-        //Mettre la position de la tete
-        SetHeadPose(HeadAngle);
+		//Ajuster les bones en dessous de la tete selon la rotation du joueur
 
-    }
+		//Mettre la position de la tete
+		SetHeadPose(HeadAngle);
+
+
+	}
 }
--- dev-game/Core/World/CharacterModel/Player.cs
+++ dev-game/Core/World/CharacterModel/Player.cs
@@ -24,132 +24,152 @@ using static Utils.RayCastUtils;
 /// </summary>
 public partial class Player : CharacterBody3D
 {
-    /// ····································
-    /// : _____  _____  ___  ___ _____ ___ :
-    /// :| __\ \/ | _ \/ _ \| _ |_   _/ __|:
-    /// :| _| >  <|  _| (_) |   / | | \__ \:
-    /// :|___/_/\_|_|  \___/|_|_\ |_| |___/:
-    /// ····································
+	/// ····································
+	/// : _____  _____  ___  ___ _____ ___ :
+	/// :| __\ \/ | _ \/ _ \| _ |_   _/ __|:
+	/// :| _| >  <|  _| (_) |   / | | \__ \:
+	/// :|___/_/\_|_|  \___/|_|_\ |_| |___/:
+	/// ····································
 
-    [ExportGroup("Player Settings")]
-    [Export(PropertyHint.Range, "0.0f,10.0f,0.1f")] public float Speed = 2.5f;
-    [Export(PropertyHint.Range, "0.0f,10.0f,0.1f")] public float JumpVelocity = 4.5f;
+	[ExportGroup("Player Settings")]
+	[Export(PropertyHint.Range, "0.0f,10.0f,0.1f")] public float Speed = 2.5f;
+	[Export(PropertyHint.Range, "0.0f,10.0f,0.1f")] public float RunMultiplier = 2.0f;
+	[Export(PropertyHint.Range, "0.0f,10.0f,0.1f")] public float JumpVelocity = 4.5f;
 
-    [ExportGroup("Controller Settings")]
-    [Export(PropertyHint.Range, "0.0f,0.1f,0.001f")] private float _mouseSensitivity = 0.002f;
-    [Export(PropertyHint.Range, "0.0f,5.0f,0.05f")] private float _controllerSensitivity = 2.5f; // radians / sec
-    [ExportGroup("Character Nodes")]
-    [Export] public required AnimationPlayer AnimPlayer;
-    [Export] public required PhysicsSkeleton PhysicsSkelton;
-    [Export] required public RayCast3D Raycaster;
-    [Export] private Camera3D? _cam;
-    private float _headAngle = 0.0f;//rads
-    private float _prevAngle = 0.0f;
-    private Skeleton3D? _animSkeleton;
-    private string? _currentAnim;
-    //[Export] private Node3D _characterRig;
+	[ExportGroup("Controller Settings")]
+	[Export(PropertyHint.Range, "0.0f,0.1f,0.001f")] private float _mouseSensitivity = 0.002f;
+	[Export(PropertyHint.Range, "0.0f,5.0f,0.05f")] private float _controllerSensitivity = 2.5f; // radians / sec
+	[ExportGroup("Character Nodes")]
+	[Export] public required AnimationPlayer AnimPlayer;
+	[Export] public required PhysicsSkeleton PhysicsSkelton;
+	[Export] required public RayCast3D Raycaster;
+	[Export] private Camera3D? _cam;
+	private float _headAngle = 0.0f;//rads
+	private float _prevAngle = 0.0f;
+	private Skeleton3D? _animSkeleton;
+	private string? _currentAnim;
+	private Vector3 _offsetFP=new Vector3(0, 0.05f, 0.25f);
+	private Vector3 _offsetTP=new Vector3(0, 0.1f, -1.25f);
+	private Vector3 _currentCamOffset;
+	//[Export] private Node3D _characterRig;
 
-    public void SetAnimation(string anim)
-    {
-        if (_currentAnim != anim)
-        {
-            _currentAnim = anim;
-            AnimPlayer.Play(anim);
-        }
-    }
+	public void SetAnimation(string anim)
+	{
+		if (_currentAnim != anim)
+		{
+			_currentAnim = anim;
+			AnimPlayer.Play(anim);
+		}
+	}
+
+	public void SetCamPos(){
+	int boneIdx = PhysicsSkelton.FindBone("Head.001");
+	Transform3D headWorld = PhysicsSkelton.GlobalTransform * PhysicsSkelton.GetBoneGlobalPose(boneIdx);
+	_cam.GlobalPosition = headWorld.Origin + headWorld.Basis *_currentCamOffset;
+	}
+
+	/// ···········································
+	/// : _    ___ ___ ___ _____   _____ _    ___ :
+	/// :| |  |_ _| __| __/ __\ \ / / __| |  | __|:
+	/// :| |__ | || _|| _| (__ \ V | (__| |__| _| :
+	/// :|____|___|_| |___\___| |_| \___|____|___|:
+	/// ···········································
+	public override void _Ready()
+	{
+		if (_cam == null) return;
+		_currentCamOffset = _offsetTP;
+		if (Raycaster == null) Raycaster = GetNode<RayCast3D>("PlayerCamera_FP/RayCast3D");
+		Input.MouseMode = Input.MouseModeEnum.Captured; //Cache le curseur à son controle
+														//TODO: Mettre dans un médiateur de contrôle
+														// pour permettre de lose control
+		if (PhysicsSkelton == null) PhysicsSkelton = GetNode<PhysicsSkeleton>("PhysicsRig/Armature/Skeleton3D");
+		if (AnimPlayer == null) AnimPlayer = PhysicsSkelton.AnimPlayer;
+		_animSkeleton = PhysicsSkelton.TargetSkeleton;
+
+	}
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseMotion mouseMotion)
+		{
+			//Rotate le player en horizontal complet (on fera la tete plus tard isolé)
+			RotateY(-mouseMotion.Relative.X * _mouseSensitivity);
+
+			// Vertical tourne seulement la caméra pour l'instant
+			_cam.RotateX(mouseMotion.Relative.Y * _mouseSensitivity);
+			_cam.Rotation = new Vector3(
+				Mathf.Clamp(_cam.Rotation.X, Mathf.DegToRad(-80), Mathf.DegToRad(80)),
+				_cam.Rotation.Y,
+				_cam.Rotation.Z
+			);
+			//tourner la tete sur laxe x (les spine bones sont dans le process de PhysicsSkeleton.cs)
+			_prevAngle = _headAngle;
+			_headAngle = _cam.Rotation.X;
+		}
+		else if (@event is InputEventAction action)
+		{
+
+		}
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+		if (Input.IsActionJustPressed("run")){
+			Speed *=RunMultiplier;
+			AnimPlayer.SpeedScale *=RunMultiplier;
+		}
+		if(Input.IsActionJustReleased("run")){
+			Speed/=RunMultiplier;
+			AnimPlayer.SpeedScale /=RunMultiplier;
+
+		}
+
+		if (Input.IsActionJustPressed("interact"))
+		{
+
+			GetObjectTypeFromRaycast(Raycaster);
+			//TEMPORAIRE : Changer la camera a third person pour voir le rig
+			//_currentCamOffset = (_currentCamOffset == _offsetFP)?_offsetTP:_offsetFP;
 
 
-    /// ···········································
-    /// : _    ___ ___ ___ _____   _____ _    ___ :
-    /// :| |  |_ _| __| __/ __\ \ / / __| |  | __|:
-    /// :| |__ | || _|| _| (__ \ V | (__| |__| _| :
-    /// :|____|___|_| |___\___| |_| \___|____|___|:
-    /// ···········································
-    public override void _Ready()
-    {
-        if (_cam == null) return;
+		}
 
-        if (Raycaster == null) Raycaster = GetNode<RayCast3D>("PlayerCamera_FP/RayCast3D");
-        Input.MouseMode = Input.MouseModeEnum.Captured; //Cache le curseur à son controle
-                                                        //TODO: Mettre dans un médiateur de contrôle
-                                                        // pour permettre de lose control
-        if (PhysicsSkelton == null) PhysicsSkelton = GetNode<PhysicsSkeleton>("PhysicsRig/Armature/Skeleton3D");
-        if (AnimPlayer == null) AnimPlayer = PhysicsSkelton.AnimPlayer;
-        _animSkeleton = PhysicsSkelton.TargetSkeleton;
+		// Basic movements from the godot boilerplate, to adapt to the game
+		Vector2 aimDir = Input.GetVector("aim_left", "aim_right", "aim_up", "aim_down");
+		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		Vector3 direction = (Transform.Basis * new Vector3(-inputDir.X, 0, -inputDir.Y)).Normalized();
+		//Gérer le aiming (controller ou souris)
 
-    }
-    public override void _Input(InputEvent @event)
-    {
-        if (@event is InputEventMouseMotion mouseMotion)
-        {
-            //Rotate le player en horizontal complet (on fera la tete plus tard isolé)
-            RotateY(-mouseMotion.Relative.X * _mouseSensitivity);
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+			SetAnimation("WalkAction_001");
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+			SetAnimation("Idle_001");
+		}
+		PhysicsSkelton.HeadAngle = _headAngle;
 
-            // Vertical tourne seulement la caméra pour l'instant
-            _cam.RotateX(mouseMotion.Relative.Y * _mouseSensitivity);
-            _cam.Rotation = new Vector3(
-                Mathf.Clamp(_cam.Rotation.X, Mathf.DegToRad(-80), Mathf.DegToRad(80)),
-                _cam.Rotation.Y,
-                _cam.Rotation.Z
-            );
-            //tourner la tete sur laxe x (les spine bones sont dans le process de PhysicsSkeleton.cs)
-            _prevAngle = _headAngle;
-            _headAngle = _cam.Rotation.X;
-        }
-        else if (@event is InputEventAction action)
-        {
-
-        }
-    }
-    public override void _PhysicsProcess(double delta)
-    {
-        Vector3 velocity = Velocity;
-
-        // Add the gravity.
-        if (!IsOnFloor())
-        {
-            velocity += GetGravity() * (float)delta;
-        }
-
-        // Handle Jump.
-        if (Input.IsActionJustPressed("jump") && IsOnFloor())
-        {
-            velocity.Y = JumpVelocity;
-        }
-
-        if (Input.IsActionJustPressed("interact"))
-        {
-
-            GetObjectTypeFromRaycast(Raycaster);
-        }
-
-        // Basic movements from the godot boilerplate, to adapt to the game
-        Vector2 aimDir = Input.GetVector("aim_left", "aim_right", "aim_up", "aim_down");
-        Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, -inputDir.Y)).Normalized();
-        //Gérer le aiming (controller ou souris)
-
-        if (direction != Vector3.Zero)
-        {
-            velocity.X = direction.X * Speed;
-            velocity.Z = direction.Z * Speed;
-            SetAnimation("WalkAction_001");
-        }
-        else
-        {
-            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-            SetAnimation("Idle_001");
-        }
-        PhysicsSkelton.HeadAngle = _headAngle;
-
-        Velocity = velocity;
-        MoveAndSlide();
-    }
-    public override void _Process(double delta)
-    {
-        int boneIdx = PhysicsSkelton.FindBone("Head.001");
-        Transform3D headWorld = PhysicsSkelton.GlobalTransform * PhysicsSkelton.GetBoneGlobalPose(boneIdx);
-        _cam.GlobalPosition = headWorld.Origin + headWorld.Basis * new Vector3(0, 0.05f, 0.25f); //TODO: mettre offset dans var
-    }
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+	public override void _Process(double delta)
+	{
+		SetCamPos();
+	}
 }
