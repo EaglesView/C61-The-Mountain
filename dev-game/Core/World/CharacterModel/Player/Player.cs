4a017b2bd058489b3fae72dd370009021df4ad1a
jim <bouchardplatre@gmail.com>
2026-03-26 13:03:10 -0400

    MOVE: Deplacement Character et player dans dossiers

--- dev-game/Core/World/CharacterModel/Player/Player.cs
+++ dev-game/Core/World/CharacterModel/Player/Player.cs
@@ -27,7 +27,7 @@ using static Utils.CharacterUtils;
 /// </summary>
 public partial class Player : Character
 {
-    /*
+	/*
 	····································
 	: _____  _____  ___  ___ _____ ___ :
 	:| __\ \/ | _ \/ _ \| _ |_   _/ __|:
@@ -36,69 +36,69 @@ public partial class Player : Character
 	····································
 	*/
 
-    [ExportGroup("Player Settings")]
-    ///<summary> La vitesse de marche du personnage joué</summary>
-    [Export(PropertyHint.Range, "0.0f,10.0f,0.1f")] public float WalkSpeed = 2.5f;
-    ///<summary> Le multiplicateur de vitesse de la course </summary>
-    [Export(PropertyHint.Range, "0.0f,10.0f,0.1f")] public float RunMultiplier = 2.0f;
-    ///<summary>La force de saut du personnage</summary>
-    [Export(PropertyHint.Range, "0.0f,10.0f,0.1f")] public float JumpVelocity = 4.5f;
+	[ExportGroup("Player Settings")]
+	///<summary> La vitesse de marche du personnage joué</summary>
+	[Export(PropertyHint.Range, "0.0f,10.0f,0.1f")] public float WalkSpeed = 2.5f;
+	///<summary> Le multiplicateur de vitesse de la course </summary>
+	[Export(PropertyHint.Range, "0.0f,10.0f,0.1f")] public float RunMultiplier = 2.0f;
+	///<summary>La force de saut du personnage</summary>
+	[Export(PropertyHint.Range, "0.0f,10.0f,0.1f")] public float JumpVelocity = 4.5f;
 
-    [ExportGroup("Controller Settings")]
-    [Export(PropertyHint.Range, "0.0f,0.1f,0.001f")] private float _mouseSensitivity = 0.002f;
-    [Export(PropertyHint.Range, "0.0f,5.0f,0.05f")] private float _controllerSensitivity = 2.5f; // radians / sec
-    [ExportGroup("Character Nodes")]
-    [Export] private CameraMan? _cameraMan;
-    [Export]
-    ///<summary> Le raycaster du joueur pour detecter les objets</summary>
-    public RayCast3D Raycaster;
-    //[Export] public required AnimationPlayer AnimPlayer;
-    //[Export] public required PhysicsSkeleton PhysicsSkelton;
-    //[Export] private Camera3D? _cam;
-    private Skeleton3D? _animSkeleton;
-    private Vector3 _offsetFP = new Vector3(0, 0.05f, 0.25f);
-    private Vector3 _offsetTP = new Vector3(0, 0.1f, -1.25f);
-    private Vector3 _currentCamOffset;
-    private string? _currentAnim;
-    private Interactable? _highlightedInteractable;
-    //[Export] private Node3D _characterRig;
-    private bool _showDebug = false;
-    private bool _playerFocused = false;
-    private bool _playerPaused = false;
-    //public void SetCamPos()
-    //{
-    //    int boneIdx = PhysicsSkelton.FindBone("Head.001");
-    //    Transform3D headWorld = PhysicsSkelton.GlobalTransform * PhysicsSkelton.GetBoneGlobalPose(boneIdx);
-    //    _cam.GlobalPosition = headWorld.Origin + headWorld.Basis * _currentCamOffset;
-    //}
+	[ExportGroup("Controller Settings")]
+	[Export(PropertyHint.Range, "0.0f,0.1f,0.001f")] private float _mouseSensitivity = 0.002f;
+	[Export(PropertyHint.Range, "0.0f,5.0f,0.05f")] private float _controllerSensitivity = 2.5f; // radians / sec
+	[ExportGroup("Character Nodes")]
+	[Export] private CameraMan? _cameraMan;
+	[Export]
+	///<summary> Le raycaster du joueur pour detecter les objets</summary>
+	public RayCast3D Raycaster;
+	//[Export] public required AnimationPlayer AnimPlayer;
+	//[Export] public required PhysicsSkeleton PhysicsSkelton;
+	//[Export] private Camera3D? _cam;
+	private Skeleton3D? _animSkeleton;
+	private Vector3 _offsetFP = new Vector3(0, 0.05f, 0.25f);
+	private Vector3 _offsetTP = new Vector3(0, 0.1f, -1.25f);
+	private Vector3 _currentCamOffset;
+	private string? _currentAnim;
+	private Interactable? _highlightedInteractable;
+	//[Export] private Node3D _characterRig;
+	private bool _showDebug = false;
+	private bool _playerFocused = false;
+	private bool _playerPaused = false;
+	//public void SetCamPos()
+	//{
+	//    int boneIdx = PhysicsSkelton.FindBone("Head.001");
+	//    Transform3D headWorld = PhysicsSkelton.GlobalTransform * PhysicsSkelton.GetBoneGlobalPose(boneIdx);
+	//    _cam.GlobalPosition = headWorld.Origin + headWorld.Basis * _currentCamOffset;
+	//}
 
-    /// ···········································
-    /// : _    ___ ___ ___ _____   _____ _    ___ :
-    /// :| |  |_ _| __| __/ __\ \ / / __| |  | __|:
-    /// :| |__ | || _|| _| (__ \ V | (__| |__| _| :
-    /// :|____|___|_| |___\___| |_| \___|____|___|:
-    /// ···········································
-    public override void _Ready()
-    {
-        PeerId = Multiplayer.GetUniqueId();
+	/// ···········································
+	/// : _    ___ ___ ___ _____   _____ _    ___ :
+	/// :| |  |_ _| __| __/ __\ \ / / __| |  | __|:
+	/// :| |__ | || _|| _| (__ \ V | (__| |__| _| :
+	/// :|____|___|_| |___\___| |_| \___|____|___|:
+	/// ···········································
+	public override void _Ready()
+	{
+		PeerId = Multiplayer.GetUniqueId();
 
-        if (_cameraMan == null) return;
-        _currentCamOffset = _offsetFP;
-        if (Raycaster == null) Raycaster = _cameraMan.GetNode<RayCast3D>("SpringArm3D/PlayerCamera/RayCastTo");
-        if (DisplayServer.GetName() != "headless")
-            _playerFocused = ToggleCharacterFocus(_playerFocused);
-        //TODO: Mettre dans un médiateur de contrôle
-        // pour permettre de lose control
-        if (PhysicsSkelton == null) PhysicsSkelton = GetNode<PhysicsSkeleton>("PhysicsRig/Armature/Skeleton3D");
-        if (AnimPlayer == null) AnimPlayer = PhysicsSkelton.AnimPlayer;
-        _animSkeleton = PhysicsSkelton.TargetSkeleton;
-        speed = WalkSpeed;
-        AddToGroup("local_player");
+		if (_cameraMan == null) return;
+		_currentCamOffset = _offsetFP;
+		if (Raycaster == null) Raycaster = _cameraMan.GetNode<RayCast3D>("SpringArm3D/PlayerCamera/RayCastTo");
+		if (DisplayServer.GetName() != "headless")
+			_playerFocused = ToggleCharacterFocus(_playerFocused);
+		//TODO: Mettre dans un médiateur de contrôle
+		// pour permettre de lose control
+		if (PhysicsSkelton == null) PhysicsSkelton = GetNode<PhysicsSkeleton>("PhysicsRig/Armature/Skeleton3D");
+		if (AnimPlayer == null) AnimPlayer = PhysicsSkelton.AnimPlayer;
+		_animSkeleton = PhysicsSkelton.TargetSkeleton;
+		speed = WalkSpeed;
+		AddToGroup("local_player");
 
-    }
-    public override void _Input(InputEvent @event)
-    {
-        // Bloque tout l'input du joueur quand le menu de pause est ouvert
+	}
+	public override void _Input(InputEvent @event)
+	{
+		// Bloque tout l'input du joueur quand le menu de pause est ouvert
         if (GameMenu.IsPaused) return;
 
         if (@event is InputEventMouseMotion mouseMotion)
