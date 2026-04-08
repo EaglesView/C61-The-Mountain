0ae25761a62d4fd1cc0034420dbbff913e0b7e2d
jim <bouchardplatre@gmail.com>
2026-04-08 09:48:56 -0400

    fix debug ui pour fier sur playerspawner

--- dev-game/Core/UI/Debug/Debug.cs
+++ dev-game/Core/UI/Debug/Debug.cs
@@ -24,7 +24,7 @@ public partial class Debug : Control
 	/// :| _| >  <|  _| (_) |   / | | \__ \:
 	/// :|___/_/\_|_|  \___/|_|_\ |_| |___/:
 	/// ····································
-	[Export] private Player _player;
+	private Player? _player;
 	[ExportCategory("Internal")]
 	[Export] private Label _fpsValue;
 
@@ -35,9 +35,14 @@ public partial class Debug : Control
 	[Export] private Label _pingValue;
 	[Export] private Label _positionXLabel;
 	[Export] private Label _positionYLabel;
+	[Export] private Label _positionZLabel;
 	[Export] private Label _headAngleRadLabel;
 	[Export] private Label _headAngleDegLabel;
 	[Export] private Label _velocityLabel;
+	[Export] private Label _speedLabel;
+	[Export] private Label _characterStateLabel;
+	[Export] private Label _isOnFloorLabel;
+	[Export] private Label _isRagdollLabel;
 
 	/// ···········································
 	/// : _    ___ ___ ___ _____   _____ _    ___ :
@@ -56,11 +61,24 @@ public partial class Debug : Control
 		if (!Visible) return;
 
 		_fpsValue.Text = Engine.GetFramesPerSecond().ToString();
-		_positionXLabel.Text = _player.GlobalPosition.X.ToString();
-		_positionYLabel.Text = _player.GlobalPosition.Y.ToString();
-		_headAngleRadLabel.Text = (-_player.GetHeadAngle()).ToString();
-		_headAngleDegLabel.Text = (-_player.GetHeadAngle() * (180.0f / Mathf.Pi)).ToString();
+
+		if (_player == null)
+		{
+			var group = GetTree().GetNodesInGroup("local_player");
+			if (group.Count == 0) return;
+			_player = group[0] as Player;
+			if (_player == null) return;
+		}
+		_positionXLabel.Text = _player.GlobalPosition.X.ToString("F3");
+		_positionYLabel.Text = _player.GlobalPosition.Y.ToString("F3");
+		_positionZLabel.Text = _player.GlobalPosition.Z.ToString("F3");
+		_headAngleRadLabel.Text = (-_player.GetHeadAngle()).ToString("F3");
+		_headAngleDegLabel.Text = (-_player.GetHeadAngle() * (180.0f / Mathf.Pi)).ToString("F1");
 		_velocityLabel.Text = _player.Velocity.ToString();
+		_speedLabel.Text = _player.Velocity.Length().ToString("F2");
+		_characterStateLabel.Text = _player.GetCurrentState().ToString();
+		_isOnFloorLabel.Text = _player.IsOnFloor().ToString();
+		_isRagdollLabel.Text = _player.PhysicsSkelton.IsRagdoll.ToString();
 
 		var peer = Multiplayer.MultiplayerPeer;
 		bool connected = peer != null && peer.GetConnectionStatus() == MultiplayerPeer.ConnectionStatus.Connected;
